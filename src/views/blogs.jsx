cb266c7df36aa1cf561bb2f497dcfc569d9e8cb2
comanchechao <61477751+comanchechao@users.noreply.github.com>
2022-12-21 11:02:49 +0330

    Update blogs.jsx

--- src/views/blogs.jsx
+++ src/views/blogs.jsx
@@ -14,32 +14,63 @@ export default function blogs() {
   return (
     <div className=" w-full  h-full bg-mainWhite">
       <Navbar />
-      <div className="p-5 flex flex-col space-y-5 w-full h-full">
-        <div className="mt-16 space-y-6 flex justify-end items-center flex-col  align-center">
+      <div className="p-5 flex flex-col space-y-5 w-screen h-full items-center bg-CoolGray-900">
+        <div className="mt-16 space-y-6 flex justify-end items-center flex-col   align-center">
           <div className="flex space-y-2 flex-col justify-center align-center items-center">
-            <h1 className="font-bold capitalize my-7 text-7xl text-center">
-              {t("blogPageHeader")}
+            <h1 className="font-bold text-mainWhite capitalize leading-tight mt-7 text-10xl text-center">
+              Azim Blog
             </h1>
-            <h2 className="text-xl">Monday 2022</h2>
+            <h2 className="text-xl text-mainWhite ">Monday 2022</h2>
           </div>
-          <div className="flex justify-center">
-            <img
-              className="object-contain w-full h-full lg:w-1/2 lg:h-1/2"
-              src={nordGlasses}
-              alt=""
-            />
-          </div>
-          <div className="flex w-full justify-center items-center flex-col space-y-8">
-            <input
-              className="p-4 w-80 rounded-full text-xl placeholder-gray-900"
-              placeholder="Search Articles..."
-              type="text"
-            />
-            <h2 className="text-5xl font-bold">{t("recentArticles")}</h2>
-          </div>
-          <div className="bg-white p-5 rounded-full">
+          {/* <div className="bg-white p-5 rounded-full">
             <DotsThreeOutlineVertical size={40} />
+          </div> */}
+        </div>
+        <div className="w-screen h-carousel px-32 p-10 flex items-center bg-CoolGray-800">
+          <div className="w-1/2 h-full text-mainWhite  flex items-start flex-col  text-left">
+            <img className="  object-contain h-96" src={nordGlasses} alt="" />
+            <div className="flex items-start mt-2 flex-col space-y-3">
+              <h1 className="text-4xl font-bold pr-40">
+                Lorem ipsum dolor sit amet consectetur
+              </h1>
+              <h3 className="text-lg font-light pr-40">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Tempore inventore a iusto nemo, asperiores quod veniam excepturi
+                totam sit debitis magnam et accusamus dolores quo molestias
+                voluptatibus atque. Delectus, tenetur.
+              </h3>
+            </div>
           </div>
+          <div className="w-1/2 h-full flex flex-col justify-center space-y-6 text-mainWhite items-center ">
+            <div className="flex items-center justify-center space-x-5">
+              <img className="  object-contain h-40" src={nordGlasses} alt="" />
+
+              <h1 className="text-4xl font-bold pr-40">
+                Lorem ipsum dolor sit amet consectetur
+              </h1>
+            </div>
+            <div className="flex items-center justify-center space-x-5">
+              <img className="  object-contain h-40" src={nordGlasses} alt="" />
+
+              <h1 className="text-4xl font-bold pr-40">
+                Lorem ipsum dolor sit amet consectetur
+              </h1>
+            </div>
+            <div className="flex items-center justify-center space-x-5">
+              <img className="  object-contain h-40" src={nordGlasses} alt="" />
+
+              <h1 className="text-4xl font-bold pr-40">
+                Lorem ipsum dolor sit amet consectetur
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full justify-center items-center flex-col">
+          <input
+            className="p-4 w-80 rounded-full  mt-24 text-xl placeholder-gray-900"
+            placeholder="Search Articles..."
+            type="text"
+          />
         </div>
         <div className="w-full h-full">
           <BlogList />
