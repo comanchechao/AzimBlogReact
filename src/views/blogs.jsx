94f8c1de34366e6750ea3f8d25c2ed61600574e4
Greez <imgreez@protonmail.com>
2023-01-07 11:14:30 +0330

    deliver update

--- src/views/blogs.jsx
+++ src/views/blogs.jsx
@@ -1,12 +1,13 @@
 import Navbar from "../components/navbar";
 import nordGlasses from "../assets/images/nordGlasses.webp";
-import { DotsThreeOutlineVertical, SquaresFour, Rows } from "phosphor-react";
+import { Eyeglasses } from "phosphor-react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import { supabase } from "../supabaseClient";
 import BlogList from "../components/blogList";
 import { useTranslation } from "react-i18next";
+import logo from "../assets/images/logo.png";
 
 export default function blogs() {
   const { t, i18n } = useTranslation();
@@ -15,62 +16,37 @@ export default function blogs() {
     <div className=" w-screen  h-full bg-mainWhite">
       <Navbar />
       <div className="p-5 flex flex-col space-y-5 w-screen h-full items-center bg-CoolGray">
-        <div className="mt-16 space-y-6 flex justify-end items-center flex-col   align-center">
-          <div className="flex space-y-2 flex-col justify-center align-center items-center">
+        <div className="mt-16 space-y-6 w-full flex justify-end items-center flex-col   align-center">
+          <div className="flex w-full space-y-2 flex-col justify-center align-center items-center">
+            <div className="flex w-10/12 flex-col-reverse py-5 lg:flex-row justify-around ">
+              <img
+                className="bg-mainCream p-5 rounded-full w-96 h-96 object-contain"
+                src={logo}
+                alt=""
+              />
+              <h1
+                className="font-bold text-mainWhite font-SultanFont capitalize leading-tight
+              mb-20  mt-40 text-5xl lg:text-8xl text-center"
+              >
+                فروشگاه شانل
+              </h1>
+            </div>
             <h1
               className="font-bold text-mainWhite font-SultanFont capitalize leading-tight
-              mb-20  mt-40 text-5xl lg:text-10xl text-center"
+                mt-40 text-xl lg:text-2xl text-center"
             >
-              Azim Blog
+              وبلاگ های بروز
             </h1>
           </div>
           {/* <div className="bg-white p-5 rounded-full">
             <DotsThreeOutlineVertical size={40} />
           </div> */}
         </div>
-        <div className="w-screen h-auto lg:h-carousel mt-12  lg:px-32 lg:py-24 lg:flex-row flex-col flex items-center bg-CoolGray">
-          <div className="lg:w-1/2 w-full h-auto text-mainWhite  flex items-center lg:items-start flex-col text-center lg:text-left">
-            <img className="  object-contain h-96" src={nordGlasses} alt="" />
-            <div className="flex items-center lg:items-start mt-2 flex-col space-y-3 lg:px-0 px-5">
-              <h1 className="lg:text-4xl text-2xl font-bold lg:pr-40 mt-0 lg:mt-3">
-                Lorem ipsum dolor sit amet consectetur
-              </h1>
-              <h3 className="text-lg font-light text-mainCream lg:pr-40">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Tempore inventore a iusto nemo, asperiores quod veniam excepturi
-                totam sit debitis magnam et accusamus dolores quo molestias
-                voluptatibus atque. Delectus, tenetur.
-              </h3>
-            </div>
-          </div>
-          <div className="lg:w-1/2 w-full lg:mt-0 my-8  h-auto flex flex-col justify-center space-y-6 text-mainWhite items-center ">
-            <div className="flex lg:flex-row flex-col items-center justify-center space-y-5 lg:space-x-5">
-              <img className="  object-contain h-40" src={nordGlasses} alt="" />
-
-              <h1 className="lg:text-4xl text-2xl font-bold lg:pr-40">
-                Lorem ipsum dolor sit amet consectetur
-              </h1>
-            </div>
-            <div className="flex lg:flex-row flex-col items-center justify-center space-y-5 lg:space-x-5">
-              <img className="  object-contain h-40" src={nordGlasses} alt="" />
-
-              <h1 className="lg:text-4xl text-2xl font-bold lg:pr-40">
-                Lorem ipsum dolor sit amet consectetur
-              </h1>
-            </div>
-            <div className="flex lg:flex-row flex-col items-center justify-center space-y-5 lg:space-x-5">
-              <img className="  object-contain h-40" src={nordGlasses} alt="" />
-
-              <h1 className="lg:text-4xl text-2xl font-bold lg:pr-40">
-                Lorem ipsum dolor sit amet consectetur
-              </h1>
-            </div>
-          </div>
-        </div>
+        <div className="w-screen h-auto lg:h-carousel   lg:px-32 lg:py-24 lg:flex-row flex-col flex items-center bg-CoolGray"></div>
         <div className="flex w-full justify-center items-center flex-col">
           <input
-            className="p-4 w-80 rounded-full  mt-24 text-xl placeholder-gray-900"
-            placeholder="Search Articles..."
+            className="p-4 w-80 rounded-full  mt-24 text-xl text-right placeholder-gray-900"
+            placeholder="جستوجو..."
             type="text"
           />
         </div>
